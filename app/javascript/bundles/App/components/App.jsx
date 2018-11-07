40236dcd8289ba346e2a7a09e5cfa6ace7d1804b
Haseeb Ur Rehman <you@example.com>
2018-11-07 16:40:03 -0500

    Make a main Component/routes

---
+++ app/javascript/bundles/App/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>
+          Hello
+        </h3>
+      </div>
+    );
+  }
+}
