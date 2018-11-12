3367338736733a93d6873f9f1ba7e52158c27f27
Eduard Fernandez <42663507+Bravo-Bear@users.noreply.github.com>
2018-11-12 15:49:10 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
         user.provider = auth.provider
         user.uid      = auth.uid
         user.password = Devise.friendly_token[0,20] unless user.password.present?
+        user.save
       end
   end
 end
