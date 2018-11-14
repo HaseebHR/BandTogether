a74d42a55b0876cf1bae2454bb73b75c253e5b5b
grimnia <grimnia@gmail.com>
2018-11-14 18:11:24 -0500

    add gif images

--- app/javascript/bundles/App/components/LandingPage.jsx
+++ app/javascript/bundles/App/components/LandingPage.jsx
@@ -20,7 +20,7 @@ export default class LandingPage extends Component {
         return (
           <div className="skip-main-div">
             <div className="skip-images-div">
-             <img src="/images/welcome_1.png" alt="welcome screen 1" />
+             <img src="/images/welcome_1_318x328 .gif" alt="welcome screen 1" />
             </div>
             <div className="skip-explain-div">
               <p>No move flying</p>
@@ -36,7 +36,7 @@ export default class LandingPage extends Component {
         return (
           <div className="skip-main-div">
             <div className="skip-images-div">
-             <img src="/images/welcome_2.png" alt="welcome screen 2" />
+             <img src="/images/welcome_2_318x243.gif" alt="welcome screen 2" />
             </div>
            <div className="skip-explain-div">
             <p>Select concerts</p>
@@ -52,7 +52,7 @@ export default class LandingPage extends Component {
         return (
           <div className="skip-main-div">
            <div className="skip-images-div">
-             <img className="img-responsive" src="/images/welcome_3.png" alt="welcome screen 3" />
+             <img src="/images/welcome_3_318x256.gif" alt="welcome screen 3" />
            </div>
            <div className="skip-explain-div">
              <p>Get matches that</p>
