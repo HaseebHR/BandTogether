ab36ef13ec63149fde1d018be994ad5e812fb388
Eduard Fernandez <efern218@fiu.edu>
2018-11-19 08:57:26 -0500

    THE FINAL PUSH!!!!! (hopefully)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,13 @@
 (1..10).each{|n| User.create(email: "user#{n}@user.com", password: "password" )}
 (2..10).each{|n|Like.create(liker: User.find(1), likee: User.find(n))}
 (6..10).each{|n|Like.create(liker: User.find(n), likee: User.find(1))}
+User.find(6).imgurl("https://profile-images.scdn.co/images/userprofile/default/c58ea6a9be0fabc0797f91daff2f0fb9908a4334")
+p = Profile.new
+p.user_id = User.find(1).id
+p.save
+User.find(1).profile.update(city: "miami",phone:"7865567842")
+User.find(6).profile.update(city: "miami",phone:"7865967442")
 
 puts User.all.count
 puts Like.all.count
+puts User.first.count
